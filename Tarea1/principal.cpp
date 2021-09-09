27c01af322ab58c2d4d49758f1c30d62f6cb46ef
kevin <kevin1oravl@gmail.com>
2021-09-09 16:21:08 -0400

    Agregar archivos de proyecto.

---
+++ Tarea1/principal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Mi nombre es: Medrano Cuellar Alvaro Kevin";
+	cout << "\nMi Carrera es: Ing. de Sistemas";
+	cout << "\nMi CU es: 35-4378";
+	cout << "\nMi CI es: 12705910";
+	return 0;
+}
